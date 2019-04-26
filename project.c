3d87dc8b5f1cd6d9c046227994c39a3f2d076946
Ryan Bailey <a@b.vcom>
2019-04-26 02:54:21 +0000

    All comments added to functions

--- project.c
+++ project.c
@@ -115,6 +115,8 @@ void rotCiphD(void)
         index = 0; //re-initialising index variable to 0 so decryption can be performed on all string cipher text letters. 
         printf("Please type the key number and press enter: \n");
         scanf("%d", &key); //reads the inputed key number typed by the user and stores it in the 'key' variable. 
+        
+        //The following flow control involves checking if the character is a not a letter by observing its ASCII value. Else it performs a decryption og the character. 
         while (index < stringLength) //loop that performs dencryption on each character. 'index < stringlength' is used as the index variable is initialised as '0'.
             {
             
@@ -156,90 +158,105 @@ function is limited in that the message to encrypt must have a length less than 
 void subCiphE(void)
     {
                 char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //declaring the alphabet as a string so that letters can be assigned their coresponding cipher letters. 
-                char sub[] = "QWERTYUIOPASDFGHJKLZXCVBNM"; // declaring cipher substituion alphabet so alphabetic letters can be assigned their cipher letter. 
+                char sub[1000]; // declaring cipher substituion alphabet so alphabetic letters can be assigned their cipher letter. 
                 int index = 0; // this variable is delcared so that it can be used to access elements in the alph string. 
                 
                 char message[1000]; //Declaring the message array which will be the message the user wants to encrypt. 
                 int stringLength; //declaring variable which will be used to count the length of the message string. 
                 int indexm = 0; //declaring an index variable initialised as zero so the stringlength can be counted.It will also be used to access elements of the message text. 
                 scanf("%c", message);
+                printf("Please enter sub alphabet: \n");
+                scanf("%[^\n]s", sub);
                 printf("Please type message to encrypt and press enter: \n");
                 scanf("%[^\n]s", message); //continues to scan characters until a newline character is reached. 
                 while (message[indexm] != 0) // adds to length of string until null is reached
                     {
-                        stringLength = indexm + 1;
-                        indexm++;
+                        stringLength = indexm + 1; //line that periodically counts length of the message string. '+1' is needed as index is initialed as '0'. 
+                        indexm++; //incrementing indexm before loop runs again so each index is counted. 
                     }
-                indexm = 0;
+                indexm = 0; //re-initialising indexm variable to 0 so encryption can be performed on all message characters. 
     
-                for (indexm = 0; indexm < stringLength; indexm++)
+                //The following flow control involves checking if the message character is a not a letter by observing its ASCII value. Else it performs an encryption on the character. 
+                for (indexm = 0; indexm < stringLength; indexm++) //loop that performs encryption on each character. 'indexm < stringlength' is used as the index variable is initialised as '0'. indexm is incremented after each loop so each character is executed on. 
                     {
-                      if (message[indexm] < 65 || (message[indexm] > 90 && message[indexm] < 97))
+                      if (message[indexm] < 65 || (message[indexm] > 90 && message[indexm] < 97)) //assess if message character is not a letter. 
                         {
-                            printf("%c", message[indexm]);
+                            printf("%c", message[indexm]); //prints non-letter character unmodified. '%c' converts the stored ASCII number to the character it represents.
                         }
     
-                      else 
+                      else //the following code runs if the character is a letter. 
                         {
-                            if (message[indexm] >= 97 && message[indexm] <= 122)
+                            if (message[indexm] >= 97 && message[indexm] <= 122) //assesses whether the message character us lowercase. 
                                         {
-                                            message[indexm] = message[indexm] - 32;
+                                            message[indexm] = message[indexm] - 32; //converts message character to uppercase.
                                         }
-                            for (index = 0; index < 26; index++)
+                            for (index = 0; index < 26; index++) //loop runs through each letter of the alph string and assesses if the message character matches. 
                                 {
-                                    if (message[indexm] == alph[index])
+                                    if (message[indexm] == alph[index]) //the message character is found to match the alph character. 
                                         {
-                                           printf("%c", sub[index]);
+                                           printf("%c", sub[index]); //the message character is substituted with coressponding cipher character found at the same index to the message character in the alph string. The cipher character is printed to the console. 
                                         }   
                                 }
                          }
             
                     }
-                    printf("\n");
+                    printf("\n"); //begins a new line after the cipher text.
             
-    } 
-
+    }
+    
+/* The below function implements task 4 (substitution cipher decryption given cipher text and alphabet substitution). It has no arguments or return values.
+The variables required for the function are declared within the function. The function asks the user for the cipher text to encrypt. 
+It then assesses each cipher letter individually and subs in the corresponding message letter it is assigned according to the given substitution alphabet. 
+The message letter is then printed to the console. The function also recognises any cipher characters that are not letters and thus does not perform
+the decryption on that charater (e.g. a 'space'). Any letters that are entered as lowercase are converted to uppercase to perform the decryption. The
+function is limited in that the message to encrypt must have a length less than 1000 characters. The substitution alphabet and normal alphabet are of type char.  
+*/ 
 void subCiphD(void)
     {
-        char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-                char sub[] =  "QWERTYUIOPASDFGHJKLZXCVBNM";
-                int index = 0;
+        char alph[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; //declaring the alphabet as a string so that letters can be assigned their coresponding cipher letters. 
+                char sub[] =  "QWERTYUIOPASDFGHJKLZXCVBNM"; // declaring cipher substituion alphabet so alphabetic letters can be assigned their cipher letter. 
+                int index = 0; // this variable is delcared so that it can be used to access elements in the alph string. 
     
-                char message[] = "HSTQLT utz DOSA QZ ZIT LIGHL";
+                char message[1000];
                 int stringLength;
                 int indexm = 0;
-                scanf("%[^\n]s", message); //continues to scan characters until a newline character is reached
+                
+                scanf("%c", message);
+                printf("Please type message to encrypt and press enter: \n");
+                scanf("%[^\n]s", message); //continues to scan characters until a newline character is reached. 
+                
                 while (message[indexm] != 0) // adds to length of string until null is reached
                     {
-                        stringLength = indexm + 1;
-                        indexm++;
+                        stringLength = indexm + 1; //line that periodically counts length of the cipher text string. '+1' is needed as index is initialed as '0'. 
+                        indexm++; //incrementing indexm before loop runs again so each index is counted. 
                     }
-                indexm = 0;
+                indexm = 0; //re-initialising indexm variable to 0 so decryption can be performed on all cipher characters. 
     
-                for (indexm = 0; indexm < stringLength; indexm++)
+                //The following flow control involves checking if the cipher character is a not a letter by observing its ASCII value. Else it performs a decryption on the character. 
+                for (indexm = 0; indexm < stringLength; indexm++) //loop that performs decryption on each character. 'indexm < stringlength' is used as the index variable is initialised as '0'. indexm is incremented after each loop so each character is executed on.
                     {
-                      if (message[indexm] < 65 || (message[indexm] > 90 && message[indexm] < 97))
+                      if (message[indexm] < 65 || (message[indexm] > 90 && message[indexm] < 97)) //assess if cipher character is not a letter
                         {
-                            printf("%c", message[indexm]);
+                            printf("%c", message[indexm]); //prints the non-letter character unmodified
                         }
     
-                      else 
+                      else //the following code runs if the character is a letter
                         {
-                            if (message[indexm] >= 97 && message[indexm] <= 122)
+                            if (message[indexm] >= 97 && message[indexm] <= 122) //assesses if cipher character is lowercase
                                         {
-                                            message[indexm] = message[indexm] - 32;
+                                            message[indexm] = message[indexm] - 32; //converts the lowercase letter to uppercase. 
                                         }
-                            for (index = 0; index < 26; index++)
+                            for (index = 0; index < 26; index++) //loop runs through each letter of the substitution alphabet string and assesses if the cipher character matches. 
                                 {
-                                    if (message[indexm] == sub[index])
+                                    if (message[indexm] == sub[index]) //the cipher character is found to match the substitution alphabet character.
                                         {
-                                           printf("%c", alph[index]);
+                                           printf("%c", alph[index]); //the cipher character is substituted with coressponding alphabetic character found at the same index to the message character in the substitution alphabet string. The message character is printed to the console. 
                                         }   
                                 }
                          }
             
                     }
-                    printf("\n");
+                    printf("\n"); //begins a new line after the cipher text.
 
     }
 
